707030b4e9bacba8857cf3344634e5a684e83612
hrishd <hrishd@abv.bg>
2026-06-01 09:59:14 +0300

    register user func

--- backend/users/userService.php
+++ backend/users/userService.php
@@ -11,6 +11,42 @@ class UserService
         $this->mysqli = $mysqli;
     }
 
+    public function registerUser(string $username, string $pass, string $passConf)
+    {
+        //vars
+        $usernameLen = strlen($username);
+        $passLen = strlen($pass);
+        $hashedPass = password_hash($pass, PASSWORD_DEFAULT);
+        //escape
+        $username = $this->mysqli->real_escape_string($username);
+
+        //validate 
+        //input validation
+        if ($usernameLen < 3 || $usernameLen > 20) //invalid username len
+            return ["success" => false, "error" => "Username must be between 3 and 20 characters"];
+
+        if ($passLen < 4) //invalid pass
+            return ["success" => false, "error" => "Password must be more than 4 characters"];
+
+        if ($pass != $passConf) //pass dont match
+            return ["success" => false, "error" => "Passwords don't match"];
+
+        //db validation
+        if (\getCountFromDB("users", "WHERE username = '$username'", $this->mysqli) != 0) //username is taken
+            return ["success" => false, "error" => "Username already taken"];
+
+        //insert in db
+        $stmt = $this->mysqli->prepare("INSERT INTO users (username, pass) VALUES (?, ?)");
+        $stmt->bind_param("ss", $username, $hashedPass);
+        $stmt->execute();
+        $newUserId = $stmt->insert_id;
+        $stmt->close();
+
+        //Log in the new user
+        self::logInUser($newUserId);
+
+        return ["success" => true, "error" => ""];
+    }
     public function validateAndLogInUser(string $username, string $pass)
     {
         //Select the user by username
@@ -26,17 +62,17 @@ class UserService
             $stmt->close();
 
             if (!password_verify($pass, $userRow["pass"])) //passwords dont match
-                return [0, "Incorrect username or password"];
+                return ["success" => false, "error" => "Incorrect username or password"];
 
             //log in user
             self::logInUser($userRow["id"]);
 
-            return [1, ""];
+            return ["success" => true, "error" => ""];
         }
 
         //User wasnt found
         $stmt->close();
-        return [0, "Incorrect username or password"];
+        return ["success" => false, "error" => "Incorrect username or password"];
     }
 
     private function logInUser(int $userId)
