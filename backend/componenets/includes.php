b0e28cfcc723c7cadaf94dc8ef0dbafeb156d734
hrishd <hrishd@abv.bg>
2026-06-01 18:52:26 +0300

    db utils in includes

--- backend/componenets/includes.php
+++ backend/componenets/includes.php
@@ -3,3 +3,6 @@
 /* Configs */
 require_once __DIR__ . "/../config/dbConfig.php";
 require_once __DIR__ . "/../config/sessionConfig.php";
+
+/* Utils */
+require_once __DIR__ . "/../utils/dbUtils.php";
